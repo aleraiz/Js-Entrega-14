8ed3ce327dd324c40a5d01077f89eeb019ba3963
aleraiz <alejandroraiz7@gmail.com>
2021-11-04 14:35:26 -0300

    commit

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-$( document ).ready(function() {
-    //Declaramos la url del API
-    const APIURL = 'https://jsonplaceholder.typicode.com/posts' ; 
-    //Declaramos la información a enviar
-    const infoPost =  { nombre: "Ana", profesion: "Programadora" }
-    //Agregamos un botón con jQuery
-    $("body").prepend('<button id="btn1">ENVIAR API</button>');
-    //Escuchamos el evento click del botón agregado
-    $("#btn1").click(() => { 
-        $.ajax({
-            method: "POST",
-            url:  APIURL,
-            data: infoPost,
-            success: function(respuesta){
-                $("body").prepend(`<div>${respuesta.nombre}</div>`);
-            }
+
+
+const url = "https://fakestoreapi.com/products"
+
+$.get(url, (respuesta, estado) => {
+
+    if(estado == "success") {
+        
+        respuesta.forEach(element => {
+            $("#cards").append(
+                `
+                <div class="col-3 border border-primary">
+                    <p class="text-success">${element.title}</p>
+                    <p class="text-danger">${element.price}</p>
+                </div>
+                `
+            );
         });
-    });
-});
+    }
+})
